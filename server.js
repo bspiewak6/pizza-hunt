08c4d4c45b59f1c720be7408b3740f18739d5c54
Spiewak <bspiewak6@gmail.com>
2020-12-08 21:25:23 -0500

    updated replies for comments and front-end functionality

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 // use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Connected on http://localhost:${PORT}`));
+
